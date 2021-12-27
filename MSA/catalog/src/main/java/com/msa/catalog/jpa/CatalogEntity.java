160aea23678f62868c27aa092be7d5703610441e
DESKTOP-507JULO\manbou <tjddn0197@gmail.com>
2021-12-27 13:45:50 +0900

    catalog add
    catalog 기본 entity 및 dto 추가
    yml file 추가
    data.sql을 통한 실행 시, 자동 삽입

---
+++ MSA/catalog/src/main/java/com/msa/catalog/jpa/CatalogEntity.java
@@ -0,0 +1,35 @@
+package com.msa.catalog.jpa;
+
+import lombok.Data;
+import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "catalog")
+public class CatalogEntity implements Serializable {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 120, unique = true)
+    private String productId;
+    @Column(nullable = false)
+    private String productName;
+    @Column(nullable = false)
+    private Integer Stock;
+    @Column(nullable = false)
+    private Integer unitPrice;
+
+
+    @Column(nullable = false,updatable = false,insertable = false)
+    @ColumnDefault(value = "CURRENT_TIMESTAMP")
+    private Date createAt;
+
+
+}
---
+++ MSA/catalog/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert  into catalog(prduct_id, product_name, stock, unit_price)
+    values ('catalog-001','Berlin',100,1500);
+insert  into catalog(prduct_id, product_name, stock, unit_price)
+values ('catalog-002','Tokyo',110,1000);
+insert  into catalog(prduct_id, product_name, stock, unit_price)
+values ('catalog-003','Stockholm',120,2000);
