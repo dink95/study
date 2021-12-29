8bd76e91fcc01cdd736389b486b369b0e03b7c35
DESKTOP-507JULO\manbou <tjddn0197@gmail.com>
2021-12-29 16:42:05 +0900

    config add

--- MSA/user/src/main/java/com/msa/user/config/AuthenticationFilter.java
+++ MSA/user/src/main/java/com/msa/user/config/AuthenticationFilter.java
@@ -1,47 +1,47 @@
-package com.msa.user.config;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.msa.user.vo.RequestLogin;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-
-import javax.servlet.FilterChain;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.util.ArrayList;
-
-public class AuthenticationFilter  {
-
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request,
-                                                HttpServletResponse response) throws AuthenticationException {
-
-        try {
-            RequestLogin creds = new ObjectMapper().readValue(request.getInputStream(),RequestLogin.class);
-
-            return getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken
-                    (creds.getEmail(),creds.getPassword(),new ArrayList<>()
-                    )
-            );
-
-        }catch (IOException e){
-            throw new RuntimeException(e);
-        }
-
-
-
-    }
-
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain chain,
-                                            Authentication authResult) throws IOException, ServletException {
-
-        //성공 시, 어떤 반환이나 작업을 할 것인지
-    }
-}
+//package com.msa.user.config;
+//
+//import com.fasterxml.jackson.databind.ObjectMapper;
+//import com.msa.user.vo.RequestLogin;
+//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+//import org.springframework.security.core.Authentication;
+//import org.springframework.security.core.AuthenticationException;
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+//
+//import javax.servlet.FilterChain;
+//import javax.servlet.ServletException;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.util.ArrayList;
+//
+//public class AuthenticationFilter  {
+//
+//    @Override
+//    public Authentication attemptAuthentication(HttpServletRequest request,
+//                                                HttpServletResponse response) throws AuthenticationException {
+//
+//        try {
+//            RequestLogin creds = new ObjectMapper().readValue(request.getInputStream(),RequestLogin.class);
+//
+//            return getAuthenticationManager().authenticate(new UsernamePasswordAuthenticationToken
+//                    (creds.getEmail(),creds.getPassword(),new ArrayList<>()
+//                    )
+//            );
+//
+//        }catch (IOException e){
+//            throw new RuntimeException(e);
+//        }
+//
+//
+//
+//    }
+//
+//    @Override
+//    protected void successfulAuthentication(HttpServletRequest request,
+//                                            HttpServletResponse response,
+//                                            FilterChain chain,
+//                                            Authentication authResult) throws IOException, ServletException {
+//
+//        //성공 시, 어떤 반환이나 작업을 할 것인지
+//    }
+//}
--- MSA/user/src/main/java/com/msa/user/config/WebSecurity.java
+++ MSA/user/src/main/java/com/msa/user/config/WebSecurity.java
@@ -4,10 +4,10 @@ import com.msa.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import javax.servlet.Filter;
@@ -16,44 +16,44 @@ import javax.servlet.Filter;
 public class WebSecurity   {
 
 
-    private UserService userService;
-    private BCryptPasswordEncoder bCryptPasswordEncoder;
-    private Environment env;
-
-    @Autowired
-    public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder,Environment env) {
-        this.userService = userService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-        this.env=env;
-    }
-
-    public WebSecurity(boolean disableDefaults, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
-        super(disableDefaults);
-        this.userService = userService;
-        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-//        http.authorizeRequests().antMatchers("/health_check/**").permitAll();
-        http.authorizeRequests().antMatchers("/**").hasIpAddress("localhost")
-                        .and()
-                .addFilter(AuthenticationFilter());
-
-        http.headers().frameOptions().disable();//h2-console을 보기 위해서
-
-    }
-
-    private Filter AuthenticationFilter() throws Exception {
-
-        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
-        authenticationFilter.setAuthenticationManager(authenticationManager());//security manager check
-        return authenticationFilter;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-       auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
-    }
+//    private UserService userService;
+//    private BCryptPasswordEncoder bCryptPasswordEncoder;
+//    private Environment env;
+//
+//    @Autowired
+//    public WebSecurity(UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder,Environment env) {
+//        this.userService = userService;
+//        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+//        this.env=env;
+//    }
+//
+//    public WebSecurity(boolean disableDefaults, UserService userService, BCryptPasswordEncoder bCryptPasswordEncoder) {
+//        super(disableDefaults);
+//        this.userService = userService;
+//        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+//    }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.csrf().disable();
+////        http.authorizeRequests().antMatchers("/health_check/**").permitAll();
+//        http.authorizeRequests().antMatchers("/**").hasIpAddress("localhost")
+//                        .and()
+//                .addFilter(AuthenticationFilter());
+//
+//        http.headers().frameOptions().disable();//h2-console을 보기 위해서
+//
+//    }
+//
+//    private Filter AuthenticationFilter() throws Exception {
+//
+//        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+//        authenticationFilter.setAuthenticationManager(authenticationManager());//security manager check
+//        return authenticationFilter;
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//       auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+//    }
 }
